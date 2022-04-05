7955c70f64e1961884cb37bee31d5783adac6c65
usefi-ma <usefi.ma@gmail.com>
2022-04-05 01:09:05 -0600

    adding FilloutBoard function

--- FinalProject/Board.cs
+++ FinalProject/Board.cs
@@ -6,22 +6,22 @@ namespace FinalProject
 {
     class BoardConnect4
     {
-        
+
         public char[,] Board { get; set; }
         public BoardConnect4()
         {
-            
+
             Board = new char[6, 7];
         }
         public void autoPopulateBoard()
         {
-            for(int i = 0; i<6; i++)
+            for (int i = 0; i < 6; i++)
             {
                 for (int j = 0; j < 7; j++)
                 {
-                    Board[i, j] = 'X';
+                    Board[i, j] = '#';
                 }
-                
+
             }
         }
         public void displayBoard()
@@ -42,17 +42,18 @@ namespace FinalProject
 
 
         }
+
         public bool winner(Player name, char simbol)
         {
             int count;
             bool Horizontal()
             {
-                for (int i =0; i<6; i++)
+                for (int i = 0; i < 6; i++)
                 {
                     count = 0;
                     for (int j = 0; j < 6; j++)
                     {
-                        if(Board[i,j] == simbol)
+                        if (Board[i, j] == simbol)
                         {
                             count++;
                             if (count == 4) return true;
@@ -72,11 +73,30 @@ namespace FinalProject
                 return false;
             }
 
-            
-            
 
-           
+
+
+
         }
 
+        public bool FilloutBoard(int col, char simbol)
+        {
+            bool status = false;
+            for (int row = 5; row >= 0; row--)
+            {
+                if (Board[row, col-1] == '#')
+                {
+                    Board[row, col-1] = simbol;
+                    status = true;
+                    break;
+                }
+
+            }
+            return status;
+        }
+
+
     }
+
 }
+
--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -10,21 +10,22 @@ namespace FinalProject
             do
             {
 
-     
-            Console.WriteLine("Do you want to play a new Game? 1 = Yes, 0 = No");
-            int newGame = Int32.Parse(Console.ReadLine());
-            
-            if (newGame == 0) 
-            {
-                break;
-            }
 
-            Control newBoard = new Control();
+                Console.WriteLine("Do you want to play a new Game? 1 = Yes, 0 = No");
+                int newGame = Int32.Parse(Console.ReadLine());
+
+                if (newGame == 0)
+                {
+                    break;
+                }
+
+                Control newBoard = new Control();
                 Player one = new Player();
                 Player two = new Player();
                 Console.WriteLine("Player 1 name: ");
                 string name1 = Console.ReadLine();
                 newBoard.SetPlayerName(one, name1);
+                one.Simbol = 'O';
                 Console.WriteLine("do you want to play again the Computer or a Person? 0= Person, 1 = Computer ");
                 int Player2 = Int32.Parse(Console.ReadLine());
                 if (Player2 == 0)
@@ -32,53 +33,62 @@ namespace FinalProject
                     Console.WriteLine("Player 2 name: ");
                     string name2 = Console.ReadLine();
                     newBoard.SetPlayerName(two, name2);
+                    two.Simbol = 'X';
                 }
 
-                
+
 
 
 
                 newBoard.autoPopulateBoard();
-            string ans;
-             do
-            {
-                //Console.WriteLine($"Conect 4 Game. {newBoard.Player1name} Vs {newBoard.Player2name}");
-                Console.WriteLine($"Conect 4 Game.");
-                one.DisplayPlayer("Player 1");
-                two.DisplayPlayer("Player 2");
-
-                 newBoard.displayBoard();
-                    
-
-                if (newBoard.myTurn == true)
+                string ans;
+                do
                 {
+                    //Console.WriteLine($"Conect 4 Game. {newBoard.Player1name} Vs {newBoard.Player2name}");
+                    Console.WriteLine($"Conect 4 Game.");
+                    one.DisplayPlayer("Player 1");
+                    two.DisplayPlayer("Player 2");
+
+                    newBoard.displayBoard();
+
+
+                    if (newBoard.myTurn == true)
+                    {
                         // Turno del Jugador uno
                         //one.;
-                    Console.WriteLine($"{one.PlayerName}'s turn");
-                    newBoard.whoIsThePlayer();
-                    newBoard.changeTurn();
-                }
-                else
-                {
-                    Console.WriteLine($"{two.PlayerName}'s turn");
+                        Console.WriteLine($"{one.PlayerName}'s turn");
+                        newBoard.whoIsThePlayer();
+                        newBoard.changeTurn();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{two.PlayerName}'s turn");
                         //two
-                    newBoard.whoIsThePlayer();
-                    newBoard.changeTurn();
-                }
+                        newBoard.whoIsThePlayer();
+                        newBoard.changeTurn();
+                    }
+
+                    
+                    Console.WriteLine("\nPlease Enter a column number:");
+                    int ColNumber=int.Parse(Console.ReadLine());
+                    newBoard.FilloutBoard(ColNumber, one.Simbol);
+                    newBoard.displayBoard();
 
 
-                ans = Console.ReadLine();
-                if(ans == "exit")
-                {
-                    break;
-
-                }
 
 
-                Console.Clear();
-                
-            } while (true) ;
-         } while (true);
+                    ans = Console.ReadLine();
+                    if (ans == "exit")
+                    {
+                        break;
+
+                    }
+
+
+                    Console.Clear();
+
+                } while (true);
+            } while (true);
         }
     }
 }
