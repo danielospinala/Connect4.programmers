b96e2879860efcf2b78961ba3f7eb039cf05a4e1
Daniel Ospina <danielospina@gmail.com>
2022-04-15 14:42:59 -0600

    valid if the input is a number between 1 and 7, ask for an input again if not

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -9,50 +9,47 @@ namespace FinalProject
         {
             do
             {
+                Player one = new Player();
+                Player two = new Player();
                 Console.WriteLine("Do you want to play a new Game? 1 = Yes, 0 = No");
                 Console.Write("Your answer: ");
                 int newGame = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("Test");
+                
                 if (newGame == 0)
                 {
                     break;
                 }
-                else
-                {
 
-                }
-
-                Control newBoard = new Control();
-                Player one = new Player();
-                Player two = new Player();
                 
+                Control newBoard = new Control();
+               
+                    // Set Player 1 Info
+                    Console.Write("Player 1 name: ");
+                    string name1 = Console.ReadLine();
+                    newBoard.SetPlayerInfo(one, 0, name1, "Player 1", 'O');
 
-                // Set Player 1 Info
-                Console.Write("Player 1 name: ");
-                string name1 = Console.ReadLine();
-                newBoard.SetPlayerInfo(one, 0, name1, "Player 1", 'O');
+                    //Set player1 for first player (Current Player)
+                    newBoard.currentPlayer = one;
 
-                //Set player1 for first player (Current Player)
-                newBoard.currentPlayer = one;
+                    Console.WriteLine("do you want to play again the Computer or a Person? 0= Person, 1 = Computer ");
+                    Console.Write("Your answer: ");
+                    int Player2 = Int32.Parse(Console.ReadLine());
+                    if (Player2 == 0)
+                    {
+                        //SetPlayer 2 Info
+                        Console.Write("Player 2 name: ");
+                        string name2 = Console.ReadLine();
+                        newBoard.SetPlayerInfo(two, 1, name2, "Player 2", 'X');
+                    }
 
-                Console.WriteLine("do you want to play again the Computer or a Person? 0= Person, 1 = Computer ");
-                Console.Write("Your answer: ");
-                int Player2 = Int32.Parse(Console.ReadLine());
-                if (Player2 == 0)
-                {
-                    //SetPlayer 2 Info
-                    Console.Write("Player 2 name: ");
-                    string name2 = Console.ReadLine();
-                    newBoard.SetPlayerInfo(two, 1, name2, "Player 2", 'X');
-                }
+                    // Add player inplayer list
+                    newBoard.PlayerList.Add(one);
+                    newBoard.PlayerList.Add(two);
 
-                // Add player inplayer list
-                newBoard.PlayerList.Add(one);
-                newBoard.PlayerList.Add(two);
-
-                newBoard.autoPopulateBoard();
-                //string ans;
-
+                    newBoard.autoPopulateBoard();
+                    //string ans;
+                
+                
                 do
                 {
                     // Display game message
@@ -67,9 +64,24 @@ namespace FinalProject
                     newBoard.displayBoard();
 
                     // Get player column number
-                    Console.WriteLine($"\n( {newBoard.currentPlayer.Name} ) please Enter a column number for putting your symbol: ");
-                    Console.Write("Your answer: ");
-                    int ColNumber = int.Parse(Console.ReadLine());
+
+                    int ColNumber;
+                    do
+                    {
+                        Console.WriteLine($"\n( {newBoard.currentPlayer.Name} ) please Enter a column number for putting your symbol: ");
+                        Console.Write("Your answer: ");
+                        var input = Console.ReadLine();
+                        if (Int32.TryParse(input, out int number))
+                        {
+                            ColNumber = int.Parse(input);
+                            if (ColNumber >= 1 && ColNumber <= 7)
+                            {
+                                break;
+                            }
+                        }
+                        Console.WriteLine("Select a column from 1 to 7");
+
+                    } while (true);
 
                     // Putting player symbol and check right column
                     bool ActStatus = newBoard.FilloutBoard(ColNumber, newBoard.currentPlayer.Symbol);
